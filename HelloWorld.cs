ce80ee685864afe4a3e709c42134dde2e681c81c
EgorTigr11 <150163100+EgorTigr11@users.noreply.github.com>
2023-11-14 18:15:34 +0300

    Add files via upload

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public int health = 5;
+    public int level = 2;
+    public float speed = 1.2f;
+
     // Start is called before the first frame update
     void Start()
     {
         // Printing text to the console
         print("Welcome to the game!");
+
+        // Add the health
+        health += level;
+        print("Health: " + health);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
     }
 }
